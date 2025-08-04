85c5569a792db9ed2e305a426192e06c6f2f39b1
Jazmin <2024310289@uteq.edu.mx>
2025-08-04 10:15:19 -0600

    parte 2 de la historia

--- historia.py
+++ historia.py
@@ -1,1 +1,6 @@
 print("Habia una vez un niño llamado paquito MJ")
+
+print("Que encontró una caja de música enterrada en el bosque. " 
+"Al abrirla, sonó una melodía antigua y triste… y desde ese momento, " 
+"cada noche alguien tocaba la puerta de su habitación a las 3:33 a.m., pero" 
+" al asomarse, nunca había nadie.")
